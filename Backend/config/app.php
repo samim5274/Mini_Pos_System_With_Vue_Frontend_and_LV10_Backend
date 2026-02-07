a5509b47742f7357771a61a2e496af82c42ce077
samim5274 <samim.hossen5274@gmail.com>
2026-02-07 23:45:06 +0600

    update payment getway

--- Backend/config/app.php
+++ Backend/config/app.php
@@ -56,7 +56,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
-    'frontend_url' => env('FRONTEND_URL'),
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:5173'),
 
     'asset_url' => env('ASSET_URL'),
 
